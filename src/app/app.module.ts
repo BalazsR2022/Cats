a441cdb397e82cd3bf2ee7b8efc6f6f4dc838da2
Balázs Réka <balazsreka@ktch.hu>
2024-08-05 19:07:13 +0200

    start, contact, home, user components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
