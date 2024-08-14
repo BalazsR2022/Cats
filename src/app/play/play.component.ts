a8fce21bd017a551cc0e8507c97120976b780269
Balázs Réka <balazsreka@ktch.hu>
2024-08-14 20:21:35 +0200

    gradiant added home component.css, play.cpmponent.css, navbar.component.css, play cards ready, cards animation added, texts added

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,3 +1,4 @@
+// play.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./play.component.css']
 })
 export class PlayComponent {
+  cards = [
+    { image: '/assets/Toys1.png', flipped: false, winner: false, label: 'Pick Me!' },
+    { image: '/assets/Toys2.png', flipped: false, winner: false, label: 'Choose Me!' },
+    { image: '/assets/Toys3.png', flipped: false, winner: true, label: 'Select Me!' },
+    { image: '/assets/Toys4.png', flipped: false, winner: false, label: 'Click Me!' }
+  ];
 
+  flipCard(index: number) {
+    this.cards[index].flipped = !this.cards[index].flipped;
+  }
 }
